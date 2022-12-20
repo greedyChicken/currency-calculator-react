1609d95ace70e4eb31ea89713dfb1dcf6696865b
Jan Hickiewicz <janhi@tutanota.com>
2022-12-20 13:49:17 +0100

    add Result compnent and data.js file for rates data

---
+++ src/Result/index.js
@@ -0,0 +1,18 @@
+import { rates } from "../data";
+import "../data";
+
+const Result = ({ amountToConvert, fromCurrency, toCurrency }) => {
+  return (
+    <p className="footer">
+      Result:
+      <strong>
+        {(amountToConvert >= 0
+          ? amountToConvert * rates[fromCurrency][toCurrency]
+          : 0
+        ).toFixed(2)}
+      </strong>
+    </p>
+  );
+};
+
+export default Result;
---
+++ src/data.js
@@ -0,0 +1,17 @@
+export const rates = {
+  pln: {
+    pln: 1,
+    usd: 0.23,
+    eur: 0.21,
+  },
+  usd: {
+    usd: 1,
+    pln: 4.44,
+    eur: 0.95,
+  },
+  eur: {
+    eur: 1,
+    pln: 4.67,
+    usd: 1.05,
+  },
+};
