2a673fc9bc2c37782b695823c882dfa0d5fd5539
Jan Hickiewicz <janhi@tutanota.com>
2022-12-20 13:49:44 +0100

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import Container from "./Container";
 import Header from "./Header";
 import Form from "./Form";
+import Clock from "./Clock";
 import "./index.css";
 
 function App() {
   return (
-    <Container>
-      <Header title="Currency calculator" />
-      <Form />
-    </Container>
+    <>
+      <Container>
+        <Header title="Currency calculator" />
+        <Form />
+        <Clock />
+      </Container>
+    </>
   );
 }
 
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,27 +1,11 @@
 import { useState } from "react";
+import Result from "../Result";
 import "./style.css";
 
 const Form = () => {
   const [amountToConvert, setAmountToConvert] = useState("");
   const [fromCurrency, setFromCurrency] = useState("pln");
   const [toCurrency, setToCurrency] = useState("eur");
-  const [rates] = useState({
-    pln: {
-      pln: 1,
-      usd: 0.23,
-      eur: 0.21,
-    },
-    usd: {
-      usd: 1,
-      pln: 4.44,
-      eur: 0.95,
-    },
-    eur: {
-      eur: 1,
-      pln: 4.67,
-      usd: 1.05,
-    },
-  });
 
   const onInputChange = ({ target }) => {
     setAmountToConvert(target.value);
@@ -73,15 +57,11 @@ const Form = () => {
           </label>
         </p>
       </form>
-      <p className="footer">
-        Result:
-        <strong>
-          {(amountToConvert >= 0
-            ? amountToConvert * rates[fromCurrency][toCurrency]
-            : 0
-          ).toFixed(2)}
-        </strong>
-      </p>
+      <Result
+        amountToConvert={amountToConvert}
+        fromCurrency={fromCurrency}
+        toCurrency={toCurrency}
+      />
     </>
   );
 };
