06c5025e893508c9f7d1ba206166e30e2e4dea3d
Jan Hickiewicz <janhi@tutanota.com>
2022-12-22 00:14:12 +0100

    clean file

--- src/Form/styled.js
+++ src/Form/styled.js
@@ -12,20 +12,3 @@ export const Label = styled.label`
   display: inline-block;
   margin: 0 20px 0 0;
 `;
-
-// .form__input {
-// border: 1px solid #ccc;
-// border-radius: 10px;
-// padding: 10px;
-// max-width: 300px;
-// width: 100%;
-// }
-
-// .selectCurrency {
-//     display: inline-block;
-//     margin: 0 20px 0 0;
-// }
-
-// .footer {
-//     margin: 0 0 10px;
-// }
