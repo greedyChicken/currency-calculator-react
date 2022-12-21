9697dc83c0af5672d64c19fd1b633ccba69d5fbe
Jan Hickiewicz <janhi@tutanota.com>
2022-12-22 00:08:42 +0100

    refactor Form component to use styled-components

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Result from "../Result";
-import "./style.css";
+import { FormInput, Label } from "./styled";
 
 const Form = () => {
   const [amountToConvert, setAmountToConvert] = useState("");
@@ -16,8 +16,7 @@ const Form = () => {
       <form className="form">
         <p>
           <label htmlFor="amount">
-            <input
-              className="form__input"
+            <FormInput
               type="number"
               name="amount"
               min="0"
@@ -29,7 +28,7 @@ const Form = () => {
           </label>
         </p>
         <p>
-          <label className="selectCurrency" htmlFor="fromCurrency">
+          <Label htmlFor="fromCurrency">
             From:
             <select
               type="number"
@@ -41,8 +40,8 @@ const Form = () => {
               <option value="eur">EUR</option>
               <option value="usd">USD</option>
             </select>
-          </label>
-          <label className="selectCurrency" htmlFor="toCurrency">
+          </Label>
+          <Label htmlFor="toCurrency">
             To:
             <select
               type="number"
@@ -54,7 +53,7 @@ const Form = () => {
               <option value="usd">USD</option>
               <option value="pln">PLN</option>
             </select>
-          </label>
+          </Label>
         </p>
       </form>
       <Result
---
+++ src/Form/styled.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const FormInput = styled.input`
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  padding: 10px;
+  max-width: 300px;
+  width: 100%;
+`;
+
+export const Label = styled.label`
+  display: inline-block;
+  margin: 0 20px 0 0;
+`;
+
+// .form__input {
+// border: 1px solid #ccc;
+// border-radius: 10px;
+// padding: 10px;
+// max-width: 300px;
+// width: 100%;
+// }
+
+// .selectCurrency {
+//     display: inline-block;
+//     margin: 0 20px 0 0;
+// }
+
+// .footer {
+//     margin: 0 0 10px;
+// }
