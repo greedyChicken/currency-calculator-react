82517f1c70df0dc93ae1e24dc9082a817f0b2fc9
Jan Hickiewicz <janhi@tutanota.com>
2022-12-28 16:37:34 +0100

    change clock position on top

--- src/Clock/styled.js
+++ src/Clock/styled.js
@@ -3,7 +3,9 @@ import styled from "styled-components";
 export const StyledClock = styled.span`
   display: inline-block;
   font-size: 15px;
-  color: white;
-  margin-top: 0;
-  padding: 0 0 20px;
+  color: rgb(247, 118, 143);
+  top: -40px;
+  left: 0;
+  position: absolute;
+  width: 100%;
 `;
